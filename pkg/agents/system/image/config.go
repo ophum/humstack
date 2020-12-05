6d9224d2840357342999839a44a779bea951cbb5
Takahiro Inagaki <inagaki0106@gmail.com>
2020-12-06 02:10:29 +0900

    [add] ceph imageからlocalへのimage化を対応

--- pkg/agents/system/image/config.go
+++ pkg/agents/system/image/config.go
@@ -4,8 +4,14 @@ type ImageAgentDownloadAPIConfig struct {
 	AdvertiseAddress string `yaml:"advertiseAddress"`
 	ListenPort       int32  `yaml:"listenPort"`
 }
+
+type ImageAgentCephBackendConfig struct {
+	ConfigPath string `yaml:"configPath"`
+	PoolName   string `yaml:"poolName"`
+}
 type ImageAgentConfig struct {
 	ImageDirPath        string                      `yaml:"imageDirPath"`
 	BlockStorageDirPath string                      `yaml:"blockStorageDirPath"`
 	DownloadAPI         ImageAgentDownloadAPIConfig `yaml:"downloadAPI"`
+	CephBackend         ImageAgentCephBackendConfig `yaml:"cephBackend"`
 }
